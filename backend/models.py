e6581bd8c5f4e8327063138762fb9a44efd85096
CodeWithJuha <juhacodes@gmail.com>
2022-06-02 05:31:11 -0700

    Added Models Migrations and edited the Settings file

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
+from email.mime import image
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    descpription = models.TextField()
+    image = models.FileField(upload_to='resources/product')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
